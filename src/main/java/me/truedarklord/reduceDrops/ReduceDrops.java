441e840c40865cf42f444ecca0997ce8de8a1cd8
TrueDarkLord <truedarklord.email@gmail.com>
2024-10-20 12:26:03 -0600

    Mending Support (#2)
    * Mending
    Create my own mending method based on what the wiki states mending does when it comes to xp.

--- src/main/java/me/truedarklord/reduceDrops/ReduceDrops.java
+++ src/main/java/me/truedarklord/reduceDrops/ReduceDrops.java
@@ -5,10 +5,36 @@ import me.truedarklord.reduceDrops.listeners.BlockDropItems;
 import me.truedarklord.reduceDrops.listeners.Kill;
 import org.bukkit.plugin.java.JavaPlugin;
 
+
+
 public final class ReduceDrops extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        this.getServer().getConsoleSender().sendMessage(
+                """
+  
+ §#00AA00================================§#ee2222
+  _____          _                
+ |  __ \\        | |               
+ | |__) |___  __| |_   _  ___ ___ 
+ |  _  // _ \\/ _` | | | |/ __/ _ \\
+ | | \\ \\  __/ (_| | |_| | (_|  __/
+ |_|__\\_\\___|\\__,_|\\__,_|\\___\\___|
+ |  __ \\                          
+ | |  | |_ __ ___  _ __  ___      
+ | |  | | '__/ _ \\| '_ \\/ __|     
+ | |__| | | | (_) | |_) \\__ \\     
+ |_____/|_|  \\___/| .__/|___/     
+                  | |             
+                  |_|             
+§#f5da2aBy TrueDarkLord.
+§#00AA00================================
+§#f5da2aFeel free to buy me a coffee:  §#00AA00|
+§bhttps://ko-fi.com/truedarklord §#00AA00|
+§#00AA00================================
+                        """
+        );
 
         new Reload(this);
 
